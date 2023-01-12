3b28db44fef4032a8cfb62f7e883547d276bcd6a
David Ames <davidames@Davids-MacBook-Air.local>
2023-01-12 04:11:17 -0800

    updates

--- src/EventPage.js
+++ src/EventPage.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Venue from './stories/Venue/Venue';
 import EVENTS from './data.js';
+import { Slider, Switch } from 'antd';
+import logo from './logo.png';
 import './EventPage.css';
 
 function EventPage() {
     const { id } = useParams();
     const event = EVENTS[id - 1];
+    const [disabled, setDisabled] = useState(false);
 
     if (!id || !event) {
         return (
@@ -14,15 +17,107 @@ function EventPage() {
         );
     }
 
+    function SliderTooltip(props) {
+        console.log("props:", props);
+        return (
+            <div>
+                Some Value
+            </div>
+        )
+    }
+
     return (
         <div className="EventPage">
-            <div className="EventPage-header">Event: {event.name}</div>
+            <div className="EventPage-header">
+                <a className="EventPage-logoLink" href="/">
+                    <img src={logo} className="EventPage-logo" alt="logo" />
+                </a>
+            </div>
             <div className="EventPage-container">
-                <div className="EventPage-results"></div>
+                <div className="EventPage-results">
+                    <div className="EventPage-resultsItem">
+                        <div className="EventPage-resultsItem--left">
+                            <div>Section: 331</div>
+                            <div>Row: 43</div>
+                        </div>
+                        <div className="EventPage-resultsItem--right">
+                            <div className="EventPage-resultsItem--price">
+                                <div style={{ fontWeight: 600, fontSize: "20px", marginRight: "10px" }}>$81</div>
+                                <div style={{ fontStyle: "italic", fontSize: "12px", marginRight: "10px" }}>each</div>
+                            </div>
+                            <div style={{ marginTop: "20px" }}>
+                                <button>Add to Cart</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="EventPage-resultsItem">
+                        <div className="EventPage-resultsItem--left">
+                            <div>Section: 302</div>
+                            <div>Row: 14</div>
+                        </div>
+                        <div className="EventPage-resultsItem--right">
+                            <div className="EventPage-resultsItem--price">
+                                <div style={{ fontWeight: 600, fontSize: "20px", marginRight: "10px" }}>$81</div>
+                                <div style={{ fontStyle: "italic", fontSize: "12px", marginRight: "10px" }}>each</div>
+                            </div>
+                            <div style={{ marginTop: "20px" }}>
+                                <button>Add to Cart</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="EventPage-resultsItem">
+                        <div className="EventPage-resultsItem--left">
+                            <div>Section: 303</div>
+                            <div>Row: 21</div>
+                        </div>
+                        <div className="EventPage-resultsItem--right">
+                            <div className="EventPage-resultsItem--price">
+                                <div style={{ fontWeight: 600, fontSize: "20px", marginRight: "10px" }}>$104</div>
+                                <div style={{ fontStyle: "italic", fontSize: "12px", marginRight: "10px" }}>each</div>
+                            </div>
+                            <div style={{ marginTop: "20px" }}>
+                                <button>Add to Cart</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <div className="EventPage-venueMap">
                     <Venue />
                 </div>
-                <div className="EventPage-filter"></div>
+                <div className="EventPage-filter">
+                    <div>
+                        <h2 style={{
+                            textAlign: "center",
+                            fontFamily: "Helvetica",
+                            fontWeight: 300,
+                            marginTop: "30px"
+                        }}>Filters</h2>
+                    </div>
+                    <div className="EventPage-filterItem">
+                        <div className="EventPage-filterItemLabel">Price</div>
+                        <Slider
+                            range
+                            defaultValue={[80, 500]}
+                            min={0}
+                            max={5000}
+                            tooltip={{
+                                formatter: (val) => `$${val.toLocaleString()}`
+                            }}
+                        />
+                    </div>
+                    <div className="EventPage-filterItem">
+                        <div className="EventPage-filterItemLabel">Number of Tickets</div>
+                        <div className="EventPage-numOfTickets">
+                            <button>1</button>
+                            <button>2</button>
+                            <button>3</button>
+                            <button>4</button>
+                            <button>5+</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- src/Home.js
+++ src/Home.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.png';
+import olypics from './pug-olympics.png';
+import race from './pug-race.png';
+import song from './pug-song.png';
+import cartIcon from './cart-icon.svg';
 import './Home.css';
 import Search from './stories/Search/Search';
 import EVENTS from './data.js';
 
 function Home() {
+  const [cartEmpty, setCartEmpty] = useState(true);
+
   return (
     <div className="Home">
-      <div className="Home-message">
-        Find great pug-related events
-      </div>
       <header className="Home-header">
         <div className="Home-logoWrapper">
           <img src={logo} className="Home-logo" alt="logo" />
@@ -20,10 +23,23 @@ function Home() {
             </div>
           </div>
         </div>
-        <div className="Home-nav">
-          <Search placeholder="Search" />
-        </div>
       </header>
+      <div style={{ padding: "50px", display: "flex", flexDirection: "column", margin: "auto", maxWidth: "1000px", justifyContent: "center" }}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <div style={{ color: "#5500be", textAlign: "left", marginLeft: "20px", fontSize: "22px" }}>Pug Events</div>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <div style={{ margin: "20px" }}>
+              <a href="/event/1"><img className="Home-eventImg" src={olypics} /></a>
+            </div>
+            <div style={{ margin: "20px" }}>
+              <a href="/event/2"><img className="Home-eventImg" src={race} /></a>
+            </div>
+            <div style={{ margin: "20px" }}>
+              <a href="/event/3"><img className="Home-eventImg" src={song} /></a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
