2f0d7926aa4b56aff214360317dde24f34f505a1
David Ames <davidames@Davids-MacBook-Air.local>
2023-01-11 19:45:28 -0800

    adds 6 sections

--- src/stories/Venue/Venue.jsx
+++ src/stories/Venue/Venue.jsx
@@ -5,6 +5,8 @@ import { ReactComponent as Section1 } from '../assets/sectionImages/section-1.sv
 import { ReactComponent as Section2 } from '../assets/sectionImages/section-2.svg';
 import { ReactComponent as Section3 } from '../assets/sectionImages/section-3.svg';
 import { ReactComponent as Section4 } from '../assets/sectionImages/section-4.svg';
+import { ReactComponent as Section201 } from '../assets/sectionImages/section-201.svg';
+import { ReactComponent as Section202 } from '../assets/sectionImages/section-202.svg';
 import './venue.css';
 
 export const Venue = ({ venueName, ...props }) => {
@@ -17,6 +19,8 @@ export const Venue = ({ venueName, ...props }) => {
                 <VenueSection available={true} selected={false} label="2" sectionClassName="Section2"><Section2/></VenueSection>
                 <VenueSection available={true} selected={false} label="3" sectionClassName="Section3"><Section3/></VenueSection>
                 <VenueSection available={true} selected={false} label="4" sectionClassName="Section4"><Section4/></VenueSection>
+                <VenueSection available={true} selected={false} label="201" sectionClassName="Section201"><Section201/></VenueSection>
+                <VenueSection available={true} selected={false} label="202" sectionClassName="Section202"><Section202/></VenueSection>
             </svg>
         </div>
     );
